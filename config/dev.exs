dec07dd47fcd9ca2913a08e771f6fe17f151d458
Michal Musialik <michal.musialik@netguru.co>
2017-05-09 18:08:13 +0200

    Simplify db configuration for dev

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :integrator, Integrator.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "integrator_dev",
   hostname: "localhost",
   pool_size: 10
