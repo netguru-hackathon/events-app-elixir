53794f8a794fddce40701f4c0e219490bccbc310
norbertnytko <norbert.nytko@netguru.co>
2017-05-18 15:13:47 +0200

    Add item events serializer

--- web/views/api/event_view.ex
+++ web/views/api/event_view.ex
@@ -2,15 +2,23 @@ defmodule Integrator.API.EventView do
   use Integrator.Web, :view
   use JaSerializer.PhoenixView
 
+  location "/events/:id"
   attributes [:name, :description, :inserted_at, :updated_at]
 
   has_one :organisation,
-    field: :organisation_id,
-    type: "organisation"
+    include: false,
+    identifiers: :when_included
+
+  has_many :items,
+    serializer: Integrator.API.ItemView,
+    include: false,
+    identifiers: :when_included
 
   def render("error.json", conn) do
     %{
       errors: conn.message
     }
   end
+
+  def type, do: "event"
 end
