ea7959f92b77982903dcbac5eeb5f94694bd73d8
norbertnytko <norbert.nytko@netguru.co>
2017-05-19 17:14:33 +0200

    Handle api errors

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -1,5 +1,6 @@
 defmodule Integrator.ErrorView do
   use Integrator.Web, :view
+  use JaSerializer.PhoenixView
 
   def render("404.html", _assigns) do
     "Page not found"
@@ -9,8 +10,35 @@ defmodule Integrator.ErrorView do
     "Internal server error"
   end
 
-  # In case no render clause matches or no
-  # template is found, let's render it as 500
+  def render("401.json-api", _assigns) do
+    %{title: "Unauthorized", code: 401}
+    |> JaSerializer.ErrorSerializer.format
+  end
+
+  def render("403.json-api", _assigns) do
+    %{title: "Forbidden", code: 403}
+    |> JaSerializer.ErrorSerializer.format
+  end
+
+  def render("404.json-api", _assigns) do
+    %{title: "Page not found", code: 404}
+    |> JaSerializer.ErrorSerializer.format
+  end
+
+  def render("422.json-api", _assigns) do
+    %{title: "Unprocessable entity", code: 422}
+    |> JaSerializer.ErrorSerializer.format
+  end
+
+  def render("500.json-api", _assigns) do
+    %{title: "Internal Server Error", code: 500}
+    |> JaSerializer.ErrorSerializer.format
+  end
+
+  def template_not_found(_template, assigns) do
+    render "500.json-api", assigns
+  end
+
   def template_not_found(_template, assigns) do
     render "500.html", assigns
   end
